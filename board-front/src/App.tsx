b83d5b1d085dffc0ceb8efe9ccbc2e8e346d01d6
hongkyuRyu <minsubrother@naver.com>
2023-12-26 01:50:35 +0900

    feat: board_component 추가, router설정

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
+import BoardItem from 'components/BoardListItem';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { latestBoardListMock } from 'mocks';
 
 function App() {
   return (
-    <></>
+    <Router>
+      <Routes>
+      {latestBoardListMock.map((boardListItem, index) => (
+          <Route key={index} path={`/board/${index}`} element={<BoardItem boardListItem={boardListItem} />} />
+        ))}
+        {/* 기본 경로에 대한 라우트 추가 */}
+        <Route path="/" element={<div>홈 페이지입니다.</div>} />
+    </Routes>
+    </Router>
   );
 }
 
